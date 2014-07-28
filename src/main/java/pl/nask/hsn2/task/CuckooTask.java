101b63222c183fb2e015a715a4a2efdb6f256f39
radoslawl <radoslawl@nask.pl>
2014-07-28 11:48:13 +0200

    add copyrights do not save all result files (e.g. memdump)

--- src/main/java/pl/nask/hsn2/task/CuckooTask.java
+++ src/main/java/pl/nask/hsn2/task/CuckooTask.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (c) NASK, NCSC
+ * 
+ * This file is part of HoneySpider Network 2.0.
+ * 
+ * This is a free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package pl.nask.hsn2.task;
 
 import java.io.BufferedReader;
@@ -77,7 +96,7 @@ public class CuckooTask implements Task {
 				}
 				saveReport(taskId, "html");
 				saveReport(taskId, "json");
-				saveReport(taskId, "all");
+				//saveReport(taskId, "all");
 				saveStream(new URL(cuckooURL + GET_PCAP + taskId), "cuckoo_pcap");
 				saveStream(new URL(cuckooURL + GET_SCREENSHOTS + taskId), "cuckoo_screenshots");
 			}
--- src/test/java/pl/nask/hsn2/service/task/CuckooTaskTest.java
+++ src/test/java/pl/nask/hsn2/service/task/CuckooTaskTest.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (c) NASK, NCSC
+ * 
+ * This file is part of HoneySpider Network 2.0.
+ * 
+ * This is a free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package pl.nask.hsn2.service.task;
 
 import org.testng.annotations.Test;
@@ -13,7 +32,7 @@ import pl.nask.hsn2.wrappers.ObjectDataWrapper;
 
 public class CuckooTaskTest {
 
-	@Test
+	@Test(enabled=false)
 	public void taskTest() throws ParameterException, ResourceException, StorageException, InputDataException{
 		Task task = new CuckooTask(null, null, new ObjectDataWrapper(ObjectData.getDefaultInstance()){
 			@Override
